abb6ad0febd23662da84c1fac8d334bc69ae3db2
League Student <league@WTS14-4.local>
2019-06-21 18:23:12 -0700

    completedSortedArrayChecker
    hnfgjhng

--- src/_00_Sorting_Algorithms/_00_SortedArrayCheckerTest.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayCheckerTest.java
@@ -1,6 +1,7 @@
 package _00_Sorting_Algorithms;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
