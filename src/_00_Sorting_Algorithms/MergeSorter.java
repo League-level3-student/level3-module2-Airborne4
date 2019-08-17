ff68d4190b4d2bd92cafd3f641694a57a64e1169
League Student <league@iMac-12.attlocal.net>
2019-08-16 18:31:29 -0700

    Done with sorting, on to searching
    crazy stuff

--- src/_00_Sorting_Algorithms/MergeSorter.java
+++ src/_00_Sorting_Algorithms/MergeSorter.java
@@ -4,7 +4,7 @@ public class MergeSorter extends Sorter {
 	public MergeSorter() {
 		type = "Merge";
 	}
-	
+
 	// 0. Since this sorting algorithm uses recursion,
 	// we'll make a helper method called mergeSort.
 	// Complete the steps in the mergeSort method.
@@ -13,56 +13,78 @@ public class MergeSorter extends Sorter {
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
 		// 20. call the mergeSort method with 0 and the length of the array minus one
+		mergeSort(array, 0, array.length - 1, display);
 	}
 
 	private void mergeSort(int[] array, int low, int high, SortingVisualizer display) {
 		// 1. Create a temporary integer array that is the same length as the passed in
 		// array.
-		int n1 = array.length;
-		if (low < high) {
-			
-		}
+		int[] n = new int[array.length];
 		// 2. make an if statement that checks if low is less than high
 		// and put the rest of the method inside of it
+		if (low < high) {
 
-		// 3. Create an integer called middle and set it
-		// equal to the half way point between low and high
+			int middle = ((high + low) / 2);
+			mergeSort(array, low, middle, display);
+			mergeSort(array, middle + 1, high, display);
+			
+			
 
-		// 4. call the mergeSort method with low and middle
+			// 3. Create an integer called middle and set it
+			// equal to the half way point between low and high
 
-		// 5. call the mergeSort method with middle + 1 and high
+			// 4. call the mergeSort method with low and middle
 
-		// 6. copy the elements from the array into the temporary array,
-		// but only the elements from low to high inclusive
+			// 5. call the mergeSort method with middle + 1 and high
 
-		// 7. create three integers called i, j, and k and
-		// set them equal to low, middle + 1, and low respectively
+			// 6. copy the elements from the array into the temporary array,
+			// but only the elements from low to high inclusive
 
-		// 8. while i is less than or equal to middle
-		// and j is less than or equal to high
+			// 7. create three integers called i, j, and k and
+			// set them equal to low, middle + 1, and low respectively
+			int i = low;
+			int j = middle + 1;
+			int k = low;
+			// 8. while i is less than or equal to middle
+			// and j is less than or equal to high
+			while (i <= middle && j <= high) {
+				if (n[i] <= n[j]) {
+					array[k] = n[i];
+					i++;
+				} else {
+					array[k] = n[j];
+					j++;
+					k++;
+				}
+			}
 
-		// 9. if temp array at i is less than or equal
-		// to temp array at j
+			while (i <= middle) {
+				array[k] = n[i];
+				k++;
+				i++;
+			}
+			// 9. if temp array at i is less than or equal
+			// to temp array at j
 
-		// 10. set array at k equal to temp array at i
+			// 10. set array at k equal to temp array at i
 
-		// 11. increase i by 1
+			// 11. increase i by 1
 
-		// 13. else
+			// 13. else
 
-		// 14. set array at k equal to temp array at j
+			// 14. set array at k equal to temp array at j
 
-		// 15. increase j by 1
+			// 15. increase j by 1
 
-		// 16. increase k by 1
+			// 16. increase k by 1
 
-		// 17. make a while loop that runs while i is less than or equal to middle
+			// 17. make a while loop that runs while i is less than or equal to middle
 
-		// 18. set array at k equal to temp array at i
+			// 18. set array at k equal to temp array at i
 
-		// 19. increase k and i by 1
-            
-        
+			// 19. increase k and i by 1
+
+		}
 	}
 
 }
